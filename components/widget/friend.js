842aa843161e7c2063cc0e4c39e01e1b44c64b06
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-09-21 21:12:16 +0700

    feat: add  friends component

---
+++ components/widget/friend.js
@@ -0,0 +1,71 @@
+/** @format */
+
+import styled from "styled-components";
+import { Avatar, IconButton } from "@material-ui/core";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+
+const Friend = ({ name, image, quote, isOffLine }) => {
+	return (
+		<Wrapper>
+			<div className='user-heading-s'>
+				<IconButton style={{ marginLeft: "-1rem" }}>
+					<Avatar src={image ? image : "/images/tem-img.png"} />
+				</IconButton>
+				<FiberManualRecordIcon
+					className='absolute-icon'
+					style={{ color: isOffLine ? "red" : "green" }}
+				/>
+
+				<div className='user-head'>
+					<span>{name}</span>
+					<p>{quote}</p>
+				</div>
+			</div>
+		</Wrapper>
+	);
+};
+
+export default Friend;
+
+const Wrapper = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	gap: 1.2rem;
+	cursor: pointer;
+	color: #919294;
+
+	.user-heading-s {
+		display: flex;
+		align-items: center;
+		position: relative;
+
+		.user-head {
+			margin-top: 4px;
+
+			span {
+				display: block;
+				font-size: 1rem;
+				margin-bottom: 4px;
+			}
+
+			p {
+				font-size: 0.85rem;
+				font-weight: bold;
+				color: var(--color-primary);
+			}
+		}
+	}
+
+	.absolute-icon {
+		position: absolute;
+		color: green;
+		left: 17px;
+		top: 47px;
+		font-size: 1rem;
+	}
+
+	&:hover {
+		background: #ced0d4;
+	}
+`;
